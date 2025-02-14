2a3fe7d656a79bb36a50fa3f2d98d6c0fc15cb49
ElderlyRoom7805 <jubiscleudogaymipray@gmail.com>
2025-02-14 10:18:51 -0300

    fetch: BE03-063 | 🧩 Tarefa - Estrutura do Projeto: Model finalizada

--- utils.py
+++ utils.py
@@ -2,16 +2,16 @@ from exceptions import NegativeTitlesError, InvalidYearCupError, ImpossibleTitle
 from datetime import datetime
 
 
-def data_processing(**team: dict):
+def data_processing(kwargs: dict):
     current_date = datetime.now()
-    first_cup = datetime.strptime(team["first_cup"], "%Y-%m-%d")
+    first_cup = datetime.strptime(kwargs["first_cup"], "%Y-%m-%d")
     possible_cups = round(((int(current_date.year)) - int(first_cup.year)) / 4, 0)
 
-    if team["titles"] < 0:
+    if kwargs["titles"] < 0:
         raise NegativeTitlesError("titles cannot be negative")
 
     if int(first_cup.year) < 1930 or (int(first_cup.year) - 1930) % 4 != 0:
         raise InvalidYearCupError("there was no world cup this year")
 
-    if team["titles"] > possible_cups:
+    if kwargs["titles"] > possible_cups:
         raise ImpossibleTitlesError("impossible to have more titles than disputed cups")
